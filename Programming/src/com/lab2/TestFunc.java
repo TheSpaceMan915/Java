c528c0d453c5b07cc98a3160331f527aa469e106
Brilliant <azaz848426@gmail.com>
2022-03-05 23:40:10 +0300

    Fixed something

--- Programming/src/com/lab2/TestFunc.java
+++ Programming/src/com/lab2/TestFunc.java
@@ -1,5 +1,6 @@
 package com.lab2;
 import java.util.*;
+//use Ctrl and ? to comment something
 
 public class TestFunc {
 
@@ -19,18 +20,6 @@ public class TestFunc {
             System.out.printf("%15s %30s %30s", t_arr[i],min_val_arr[i], max_val_arr[i] + '\n');
         }
         System.out.println("-----------------------------------------------------------------------------");
-
-        /* System.out.println("Byte: -128 to 127");
-        System.out.println("Short: -32768 to 32767");
-        System.out.println("Int: -2147483648 to 2147483647");
-        System.out.println("Long: -9223372036854775808 to 9223372036854775807");
-        System.out.println("Float: " + 1.4E-45 + " to " + 3.4028235E38);
-        System.out.println("Double: 4.9E-324 to 1.7976931348623157E308");
-        System.out.println("Char: 0 to 65535");*/
-    }
-
-    public static void PrintTable()
-    {
 
     }
 
@@ -63,7 +52,7 @@ public class TestFunc {
         double R = 20.5,r = 5.5;
 
         System.out.println("№3");
-        System.out.println("The distance is " + res);
+        System.out.println("The distance is " + res + "\tr = " + r + "\tR = " + R);
         if (res > R)
         {System.out.println("The object is not found");}
 
@@ -73,26 +62,13 @@ public class TestFunc {
         {System.out.println("Attention!");}
     }
 
-    public static void FindDistance2(int x,int y)
+    public static void FindDistance2(int x,int y, int R, int r)
     {
-        Scanner keyboard = new Scanner(System.in);
-        int R = 0,r = 0;
 
-        try {
-        System.out.println("Enter R");
-        R = keyboard.nextInt();
-        System.out.println("Enter r");
-        r = keyboard.nextInt();}
-        catch (InputMismatchException exp)
-        {
-            System.err.println("Invalid input. Try again");
-            exp.printStackTrace();
-            System.exit(3);
-        }
         double res = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 
         System.out.println("№4");
-        System.out.println("The distance is " + res);
+        System.out.println("The distance is " + res + "\tr = " + r + "\tR = " + R);
         if (res > R)
         {System.out.println("The object is not found");}
 
@@ -122,7 +98,6 @@ public class TestFunc {
             if(j % row == 0 && j != 0)
             {
                 System.out.println(symbol);
-                //System.out.print(i);
             }
             else
             {System.out.print(symbol + " ");}
@@ -188,49 +163,88 @@ public class TestFunc {
         System.out.println("Generally, there are " + general_counter + " symbols");
     }
 
-    public static void SearchForLetters(String str,int position,char symbol)
+    public static int LookForSubstring(String str,String sub_str)
     {
-        System.out.println("The word " + str + "starts with the index " + str.indexOf("you"));
-        System.out.println("After the index " + position + "the word you starts with the index " + str.indexOf("you",position));
-        System.out.println("The index of symbol " + symbol + " is " + str.indexOf(symbol));
+        System.out.println("№9");
+        int sub_counter = 0;
+        int index_start = str.indexOf(sub_str);
+
+        if (index_start != -1)
+        {sub_counter++;}
+        else
+        {return index_start;}  //if there are no substrings return -1
+
+        int temp_ind = 0;
+
+        while (temp_ind != -1)
+        {
+           temp_ind = str.indexOf(sub_str,index_start + 1);
+           sub_counter++;
+           index_start = temp_ind;
+        }
+
+        return sub_counter;
+    }
+
+    public static void PrintString(String str)
+    {
+        System.out.println("№10");
+        for(int i = 0; i < str.length();i++)
+        {
+            for(int k = i; k < str.length();k++)
+            {
+                System.out.print(str.charAt(k));
+            }
+            for(int j = 0; j < i; j++)
+            {
+                System.out.print(str.charAt(j));
+            }
+
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
+       /* System.out.println(args[0]);
+        System.out.println(args[0]);
+        System.out.println(args[0]);*/
 
-       PrintValuesTable();
+        PrintValuesTable();
 
-       int[] arr = {-3,2,-2,6,-8,5};
+       int[] arr = {-3,2,-2,6,-8,5,7,8,-3,10,1,-1,6,7,9,5,8,3,9,4};
        System.out.println("№2");
        System.out.println("For " + Arrays.toString(arr) +" average geometric is " + AverageGeometric(arr));
 
        FindDistance(3,6);
-       Scanner keyboard_m = new Scanner(System.in);
-       int x_k = 0,y_k = 0;
 
-      /* try {
-           System.out.println("Enter x coordinate");
-           x_k = keyboard_m.nextInt();
-           System.out.println("Enter y coordinate");
-           y_k = keyboard_m.nextInt();
-       }
-       catch (InputMismatchException exp)
-       {
-           System.err.println("Invalid input. Try again");
-           exp.printStackTrace();
-           System.exit(3);
-       }
-       FindDistance2(x_k,y_k);*/
+       int x_k = Integer.parseInt(args[0]),y_k = Integer.parseInt(args[1]);
+       int r_k = Integer.parseInt(args[2]),R_k= Integer.parseInt(args[3]);
 
-        int com_arg = Integer.parseInt(args[0]);
+       int x_k2 = Integer.parseInt(args[4]),y_k2 = Integer.parseInt(args[5]);
+       int r_k2 = Integer.parseInt(args[6]),R_k2 = Integer.parseInt(args[7]);
+
+       int x_k3 = Integer.parseInt(args[8]),y_k3 = Integer.parseInt(args[9]);
+       int r_k3 = Integer.parseInt(args[10]),R_k3 = Integer.parseInt(args[11]);
+
+        FindDistance2(x_k,y_k,R_k,r_k);
+        FindDistance2(x_k2,y_k2,R_k2,r_k2);
+        FindDistance2(x_k3,y_k3,R_k3,r_k3);
+
+
+        int com_arg = Integer.parseInt(args[4]);
         PrintCommandLine(com_arg);
 
         System.out.println("№6");
         PrintUnicodeCharacters(0x0400,16,16);
         PrintUnicodeCharacters(0x20a0,2,16);
 
+        FindCharacters("cd4Ⅳ!EⅧ");
 
-        FindCharacters("cd4!E");
+        String main_str = "People think a lot about other people";
+        String substring = "people";
+        System.out.println("There are " + LookForSubstring(main_str,substring) + " substrings in this string: " + main_str);
 
-//      SearchForLetters("where are you");  Ctrl and ?
+        PrintString("Like");
     }
 }
